50e724201ac87de95e904d10a99ac71f1b7d68f5
MinnieW <84355273+MinnieW@users.noreply.github.com>
2021-07-09 22:23:26 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,76 @@
+//Declare a class called CrewCandidate with a constructor that takes three parameters—name, mass, and scores. Note that scores will be an array of test results.
+class CrewCandidate{
+  constructor(name, mass, scores){
+    this.name = name;
+    this.mass = mass;
+    this.scores = scores
+  }
+
+  addScore(score){
+    this.scores.push(score)
+  }
+
+  average(){
+    let average = 0
+    for(let i=0; i < this.scores.length; i++){
+      average += this.scores[i];
+    }
+    average = average / this.scores.length;
+    average = Math.round(average*10)/10;
+    return average
+  }
+
+  status(){
+    let averageScore = this.average();
+    if(averageScore >= 90){
+      return 'Accepted'
+    } else if (averageScore >= 80){
+      return 'Reserve'
+    } else if (averageScore >= 70){
+      return 'Probationary'
+    } else {
+      return 'Rejected'
+    }
+  }
+}
+
+let bubba = new CrewCandidate('Bubba Bear', '135 kg', [88, 85, 90]);
+
+let merry = new CrewCandidate('Merry Maltese', '1.5 kg', [93, 88, 97]);
+
+let glad = new CrewCandidate('Glad Gator', '225 kg', [75, 78, 62]);
+
+// bubba.addScore(83)
+// console.log(bubba.scores)
+// console.log(merry.average())
+// console.log(`${merry.name} earned an average test score of ${merry.average()}% and has a status of ${merry.status()}`);
+
+// console.log(`${glad.name} earned an average test score of ${glad.average()}% and has a status of ${glad.status()}`);
+let retestForReserve = 0
+
+while (glad.average() < 80){
+  glad.addScore(83);
+  retestForReserve +=1
+}
+console.log(`${glad.name} earned an average test score of ${glad.average()}% and has a status of ${glad.status()}`);
+console.log(retestForReserve)
+
+
+
+let retestForAccepted = 0
+
+while (glad.average() < 90){
+  glad.addScore(95);
+  retestForAccepted +=1
+}
+console.log(`${glad.name} earned an average test score of ${glad.average()}% and has a status of ${glad.status()}`);
+console.log(retestForAccepted)
+
+// console.log(bubba);
+// console.log(merry);
+// console.log(glad);
+//Add methods for adding scores, averaging scores and determining candidate status as described in the studio activity.
+
+
+
+//Part 4 - Use the methods to boost Glad Gator’s status to Reserve or higher. How many tests will it take to reach Reserve status? How many to reach Accepted? Remember, scores cannot exceed 100%.
